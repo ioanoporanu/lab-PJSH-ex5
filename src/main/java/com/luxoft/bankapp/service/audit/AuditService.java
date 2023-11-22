51553356ec0752b9ecdd324e282e94bc0d951e08
oporanuioan <oporanuioan1107@gmail.com>
2023-11-22 20:28:09 +0200

    ex5-t4

--- src/main/java/com/luxoft/bankapp/service/audit/AuditService.java
+++ src/main/java/com/luxoft/bankapp/service/audit/AuditService.java
@@ -14,13 +14,15 @@ public class AuditService implements Audit
 {
     private List<AccountEvent> events;
 
+
+
     public AuditService()
     {
         this.events = new ArrayList<>(100);
     }
 
     @Override
-    @EventListener
+    @EventListener(condition = "#event.amount >= @eventConditionFilter.depositLimit")
     public void auditOperation(DepositEvent event)
     {
         events.add(event);
@@ -31,7 +33,7 @@ public class AuditService implements Audit
     }
 
     @Override
-    @EventListener
+    @EventListener(condition = "#event.amount >= @eventConditionFilter.withdrawalLimit")
     public void auditOperation(WithdrawEvent event)
     {
         events.add(event);
